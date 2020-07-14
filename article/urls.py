4f9cb6b0373a3bdb152b6fd85675cbcf9e38872b
yezigege <1757423278@qq.com>
2020-07-14 23:30:49 +0800

    增加文章详情页的支持逻辑

--- article/urls.py
+++ article/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'article'
 
 urlpatterns = [
-    path('article_list/', views.article_list, name='article_list')
+    path('article_list/', views.article_list, name='article_list'),
+    # 文章详情
+    path('article_detail/<int:id>/', views.article_detail, name='article_detail')
 ]
